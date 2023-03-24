792a7c77f372b8c7dbd07e5b58cbf7246d9c22aa
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-03-24 16:58:19 +0200

    Task 1.2: added basic Task 2 solution

---
+++ Home_task_1/Home_task_1/Image.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+
+namespace Home_task_1
+{
+    internal class Image
+    {
+        public readonly int rows;
+        public readonly int cols;
+
+        private int[,] _image;
+
+        public Image(int[,] matrix)
+        {
+            rows = matrix.GetLength(0);
+            cols = matrix.GetLength(1);
+            _image = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    _image[i, j] = matrix[i, j];
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    sb.Append($"{_image[i, j]}\t");
+                }
+                sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+
+        public LineInfo FindLongestHorizontalLine()
+        {
+            LineInfo longest = new LineInfo(
+                color: _image[0, 0],
+                start: (0, 0),
+                end: (0, 0),
+                length: 1
+            );
+
+            for (int i = 0; i < rows; i++)
+            {
+                LineInfo current = new LineInfo(
+                    color: _image[i, 0],
+                    start: (i, 0),
+                    end: (i, 0),
+                    length: 1
+                );
+
+                for (int j = 1; j < cols; j++)
+                {
+                    if (_image[i, j] == current.Color)
+                    {
+                        current.Length++;
+                        current.End = (i, j);
+                    }
+                    else
+                    {
+                        if (current.Length > longest.Length)
+                        {
+                            longest = new LineInfo(
+                                current.Color,
+                                current.Start,
+                                current.End,
+                                current.Length
+                            );
+                        }
+
+                        current = new LineInfo(
+                            color: _image[i, j],
+                            start: (i, j),
+                            end: (i, j),
+                            length: 1
+                        );
+                    }
+                }
+
+                if (current.Length > longest.Length)
+                {
+                    longest = new LineInfo(
+                        current.Color,
+                        current.Start,
+                        current.End,
+                        current.Length
+                    );
+                }
+            }
+
+            return longest;
+        }
+    }
+}
--- Home_task_1/Home_task_1/Program.cs
+++ Home_task_1/Home_task_1/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task_1_2();
+        }
+
+        static void Task_1_2()
+        {
+            List<int[,]> matrices = new List<int[,]>() {
+                new int[,]
+                {
+                    { 5, 5, 1, 2 },
+                    { 8, 8, 8, 2 },
+                },
+                new int[,]
+                {
+                    { 5, 5, 5, 2 },
+                    { 8, 8, 4, 2 },
+                },
+                new int[,]
+                {
+                    { 5, 5, 1, 2 },
+                    { 8, 8, 8, 2 },
+                    { 0, 0, 0, 0 },
+                },
+                new int[,]
+                {
+                    { 5, 5, 5, 5 },
+                    { 8, 8, 8, 2 },
+                    { 0, 0, 0, 1 },
+                },
+                new int[,]
+                {
+                    { 1, 2, 3 },
+                    { 8, 1, 4 },
+                },
+            };
+
+            var images = matrices.Select(x => new Image(x)).ToArray();
+
+            foreach (var image in images)
+            {
+                Console.WriteLine(image);
+                Console.WriteLine(image.FindLongestHorizontalLine());
+                Console.WriteLine();
+            }
         }
     }
 }
