205a9fe35dfe9cd5104ab54e57d7e71111a60386
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-03-24 14:51:47 +0200

    Task 1.1: added basic Task 1 solution

--- Home_task_1/Home_task_1/Program.cs
+++ Home_task_1/Home_task_1/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SpiralMatrix[] matrices = new SpiralMatrix[]
+            {
+                new SpiralMatrix(3, 4),
+                new SpiralMatrix(7, 5),
+                new SpiralMatrix(2, 4),
+                new SpiralMatrix(4, 2),
+                new SpiralMatrix(1, 6),
+                new SpiralMatrix(4, 1),
+                new SpiralMatrix(1, 1),
+            };
+
+            foreach (var matrix in matrices)
+            {
+                matrix.Fill();
+                Console.WriteLine(matrix);
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Home_task_1/Home_task_1/Task1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_1
+{
+    internal class SpiralMatrix
+    {
+        public readonly int rows;
+        public readonly int cols;
+
+        private int[,] _matrix;
+
+        public SpiralMatrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            _matrix = new int[rows, cols];
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    sb.Append($"{_matrix[i, j]}\t");
+                }
+                sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+
+        public void Fill()
+        {
+            List<(int, int)> visited = new List<(int, int)>();
+            char direction = 'v';
+            int value = 1;
+            int i = 0;
+            int j = 0;
+            while (visited.Count < rows * cols)
+            {
+                _matrix[i, j] = value++;
+                visited.Add((i, j));
+
+                if (!IsNextIndexValid(i, j, direction))
+                {
+                    direction = ChangeDirection(direction);
+                }
+                MoveAt(direction);
+            }
+
+            bool IsNextIndexValid(int i, int j, char direction)
+            {
+                switch (direction)
+                {
+                    case 'v': i++; break;
+                    case '>': j++; break;
+                    case '^': i--; break;
+                    case '<': j--; break;
+                    default: throw new Exception("Invalid direction");
+                }
+
+                return i >= 0 && j >= 0 && 
+                       i < rows && j < cols &&
+                       !visited.Contains((i, j));
+            }
+
+            char ChangeDirection(char direction)
+            {
+                return direction switch
+                {
+                    'v' => '>',
+                    '>' => '^',
+                    '^' => '<',
+                    '<' => 'v',
+                    _ => throw new Exception("Invalid direction")
+                };
+            }
+
+            void MoveAt(char direction)
+            {
+                switch (direction)
+                {
+                    case 'v': i++; break;
+                    case '>': j++; break;
+                    case '^': i--; break;
+                    case '<': j--; break;
+                    default: throw new Exception("Invalid direction");
+                }
+            }
+        }
+    }
+}
