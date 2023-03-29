6c5a5b8050339a646ad30d628be76beff6a61cd7
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-03-29 14:08:26 +0300

    Created new project for Home task 2

---
+++ Home_task_2/Home_task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Home_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
