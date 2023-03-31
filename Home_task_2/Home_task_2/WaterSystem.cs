207d0e0f94ae19d25a37c19ba889c26ea812e1c8
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-03-31 18:48:14 +0300

    Task 2: added WaterSystem class (abstraction between User and WaterTower)

---
+++ Home_task_2/Home_task_2/WaterSystem.cs
@@ -0,0 +1,19 @@
+﻿namespace Home_task_2
+{
+    public class WaterSystem
+    {
+        private User _user;
+        private WaterTower _tower;
+
+        public WaterSystem(User user, WaterTower tower)
+        {
+            _user = user;
+            _tower = tower;
+        }
+
+        public void RequestWaterFromTower(double amount)
+        {
+
+        }
+    }
+}
