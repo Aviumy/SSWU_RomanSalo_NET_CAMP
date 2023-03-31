c284c7c802173f46045ab046dc34486726b0026c
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-03-31 21:31:59 +0300

    Task 3.2: added initial task b) solution

--- Home_task_3/Home_task_3/Program.cs
+++ Home_task_3/Home_task_3/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task3_2b();
+        }
+
+        static void Task3_2b()
+        {
+            string[] texts =
+            {
+                "What a Wonderful Day for coding",
+                "some word another word",
+                "SOME WORD ANOTHER WORD",
+                "І також Текст Кирилицею",
+                "White       Spaces",
+                "      ",
+                "",
+            };
+
+            foreach (var text in texts)
+            {
+                Console.WriteLine(TextUtils.CapitalizedWordsCount(text));
+            }
         }
     }
 }
---
+++ Home_task_3/Home_task_3/TextUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace Home_task_3
+{
+    public class TextUtils
+    {
+        public static int CapitalizedWordsCount(string text)
+        {
+            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (var word in words)
+            {
+                if (word[0].ToString() == word[0].ToString().ToUpperInvariant())
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
