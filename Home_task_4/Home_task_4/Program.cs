dd00873b86739947a7ae80d45376a1f216455a56
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-04-08 20:57:49 +0300

    4.1: add initial solution

---
+++ Home_task_4/Home_task_4/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Home_task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_4_1();
+        }
+
+        static void Task_4_1()
+        {
+            string[] englishText = new string[]
+            {
+                "Lorem ipsum dolor sit amet. Consectetur adipiscing elit,",
+                "sed do (eiusmod) tempor? incididunt ut labore et dolore. magna ",
+                "aliqua. Ut (enim ad minim veniam, quis ",
+                "nostrud exercitation ullamco laboris ",
+                "nisi) ut aliquip! ex ea commodo consequat.",
+            };
+
+            string[] ukrainianText = new string[]
+            {
+                "Дано текст, який потрібно розмістити в колекцію стрічок. (Можна для",
+                "збереження використовувати і масив)! Текст містить речення, які не",
+                "обов’язково розташовані в одній стрічці. Речення можуть закінчуватися",
+                "крапкою, комою, знаком оклику та знаком запитання? Вважати, що цих знаків",
+                "не може бути в середині речень. У реченнях може бути текст в круглих дужках.",
+                "Потрібно знайти та видрукувати всі речення, які містять інформацію в дужках.",
+                "Вкладених круглих дужок немає.",
+                "Розв’язати задачу, не використовуючи злиття (конкатенацію) стрічок.",
+                "Продемонструвати виконання для текстів українською та англійською",
+                "мовами.",
+            };
+
+            TextAnalyzer analyzer = new TextAnalyzer(englishText);
+            string[] englishResult = analyzer.SentencesWithParenthesis();
+            foreach (var sentence in englishResult)
+            {
+                Console.WriteLine(sentence);
+            }
+
+            analyzer = new TextAnalyzer(ukrainianText);
+            string[] ukrainianResult = analyzer.SentencesWithParenthesis();
+            foreach (var sentence in ukrainianResult)
+            {
+                Console.WriteLine(sentence);
+            }
+        }
+    }
+}
---
+++ Home_task_4/Home_task_4/TextAnalyzer.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace Home_task_4
+{
+    internal class TextAnalyzer
+    {
+        private static string[] _delimiters = new string[]
+        {
+            ".", "?", "!"
+        };
+
+        private string[] _text;
+
+        public TextAnalyzer(string[] text)
+        {
+            _text = text;
+        }
+
+        public string[] SentencesWithParenthesis()
+        {
+            List<string> sentencesWithBrackets = new List<string>();
+            StringBuilder currSentence = new StringBuilder();
+            bool foundLeftBracket = false;
+            bool foundRightBracket = false;
+
+            for (int i = 0; i < _text.Length; i++)
+            {
+                for (int j = 0; j < _text[i].Length; j++)
+                {
+                    currSentence.Append(_text[i][j]);
+
+                    if (_delimiters.Any(x => x == _text[i][j].ToString()))
+                    {
+                        if (foundLeftBracket && foundRightBracket)
+                        {
+                            sentencesWithBrackets.Add(currSentence.ToString());
+                        }
+                        currSentence = new StringBuilder();
+                        foundLeftBracket = false;
+                        foundRightBracket = false;
+                    }
+                    else if (_text[i][j] == '(')
+                    {
+                        foundLeftBracket = true;
+                    }
+                    else if (_text[i][j] == ')')
+                    {
+                        foundRightBracket = true;
+                    }
+                }
+            }
+
+            return sentencesWithBrackets.ToArray();
+        }
+    }
+}
