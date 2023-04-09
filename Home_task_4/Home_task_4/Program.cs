1dfe54a101fe62e7be88925e119d08b00c6fc31c
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-04-09 18:13:49 +0300

    4.1: add one more test case

--- Home_task_4/Home_task_4/Program.cs
+++ Home_task_4/Home_task_4/Program.cs
@@ -9,41 +9,46 @@
 
         static void Task_4_1()
         {
-            string[] englishText = new string[]
+            List<string[]> texts = new List<string[]>() 
             {
-                "Lorem ipsum dolor sit amet. Consectetur adipiscing elit,",
-                "sed do (eiusmod) tempor? incididunt ut labore et dolore. magna ",
-                "aliqua. Ut (enim ad minim veniam, quis ",
-                "nostrud exercitation ullamco laboris ",
-                "nisi) ut aliquip! ex ea commodo consequat.",
+                new string[]
+                {
+                    "Lorem ipsum dolor sit amet. Consectetur adipiscing elit,",
+                    "sed do (eiusmod) tempor? incididunt ut labore et dolore. magna ",
+                    "aliqua. Ut (enim ad minim veniam, quis ",
+                    "nostrud exercitation ullamco laboris ",
+                    "nisi) ut aliquip! ex ea commodo consequat.",
+                },
+                new string[]
+                {
+                    "Дано текст, який потрібно розмістити в колекцію стрічок. (Можна для",
+                    "збереження використовувати і масив)! Текст містить речення, які не",
+                    "обов’язково розташовані в одній стрічці. Речення можуть закінчуватися",
+                    "крапкою, комою, знаком оклику та знаком запитання? Вважати, що цих знаків",
+                    "не може бути в середині речень. У реченнях може бути текст в круглих дужках.",
+                    "Потрібно знайти та видрукувати всі речення, які містять інформацію в дужках.",
+                    "Вкладених круглих дужок немає.",
+                    "Розв’язати задачу, не використовуючи злиття (конкатенацію) стрічок.",
+                    "Продемонструвати виконання для текстів українською та англійською",
+                    "мовами.",
+                },
+                new string[]
+                {
+                    "This text has no ",
+                    "parenthesis in it.",
+                    "This should return",
+                    " empty collection.",
+                },
             };
 
-            string[] ukrainianText = new string[]
+            foreach (var text in texts)
             {
-                "Дано текст, який потрібно розмістити в колекцію стрічок. (Можна для",
-                "збереження використовувати і масив)! Текст містить речення, які не",
-                "обов’язково розташовані в одній стрічці. Речення можуть закінчуватися",
-                "крапкою, комою, знаком оклику та знаком запитання? Вважати, що цих знаків",
-                "не може бути в середині речень. У реченнях може бути текст в круглих дужках.",
-                "Потрібно знайти та видрукувати всі речення, які містять інформацію в дужках.",
-                "Вкладених круглих дужок немає.",
-                "Розв’язати задачу, не використовуючи злиття (конкатенацію) стрічок.",
-                "Продемонструвати виконання для текстів українською та англійською",
-                "мовами.",
-            };
-
-            TextAnalyzer analyzer = new TextAnalyzer(englishText);
-            string[] englishResult = analyzer.SentencesWithParenthesis();
-            foreach (var sentence in englishResult)
-            {
-                Console.WriteLine(sentence);
-            }
-
-            analyzer = new TextAnalyzer(ukrainianText);
-            string[] ukrainianResult = analyzer.SentencesWithParenthesis();
-            foreach (var sentence in ukrainianResult)
-            {
-                Console.WriteLine(sentence);
+                TextAnalyzer analyzer = new TextAnalyzer(text);
+                var result = analyzer.SentencesWithParenthesis();
+                foreach (var sentence in result)
+                {
+                    Console.WriteLine(sentence);
+                }
             }
         }
     }
