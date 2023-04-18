11555c2f12c35d502c1524e1ea9a56dd0e36fa3c
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-04-18 17:02:20 +0300

    5.1: implement calculating the length of fence

--- Home_task_5/Home_task_5/Fence.cs
+++ Home_task_5/Home_task_5/Fence.cs
@@ -21,20 +21,26 @@ namespace Home_task_5
 
         public double CalculateLength()
         {
+            double DistanceBetweenTrees(Tree tree1, Tree tree2)
+            {
+                return Math.Sqrt(Math.Pow(tree1.X - tree2.X, 2) + Math.Pow(tree1.Y - tree2.Y, 2));
+            }
+
             if (HullTrees.Count <= 1)
                 return 0;
 
             double length = 0;
-            foreach (Tree tree in HullTrees)
+            for (int i = 0; i < HullTrees.Count - 1; i++)
             {
-                // TODO
+                length += DistanceBetweenTrees(HullTrees[i], HullTrees[i + 1]);
             }
+            length += DistanceBetweenTrees(HullTrees[0], HullTrees.Last());
             return length;
         }
 
         public override string ToString()
         {
-            StringBuilder result = new StringBuilder("Fence is built on such trees:\n");
+            StringBuilder result = new StringBuilder($"Fence with length {Math.Round(Length, 2)} is built on such trees:\n");
             foreach (Tree tree in HullTrees)
             {
                 result.AppendLine(tree.ToString());
