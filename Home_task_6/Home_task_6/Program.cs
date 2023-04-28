f8e6b8d7ffca1b6b401f1a3a1ab3023938f5b120
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-04-28 17:47:26 +0300

    6.3: add initial solution

--- Home_task_6/Home_task_6/Program.cs
+++ Home_task_6/Home_task_6/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task6_3();
+        }
+
+        static void Task6_3()
+        {
+            Text text1 = new Text("one two three one four four two");
+            foreach (var word in text1.UniqueWords())
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Home_task_6/Home_task_6/Text.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_6
+{
+    internal class Text
+    {
+        private string _text;
+
+        public Text(string text)
+        {
+            _text = text;
+        }
+
+        public IEnumerable<string> UniqueWords()
+        {
+            HashSet<string> uniqueWords = new HashSet<string>();
+            string[] words = _text.Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (uniqueWords.Add(word))
+                {
+                    yield return word;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return _text;
+        }
+    }
+}
