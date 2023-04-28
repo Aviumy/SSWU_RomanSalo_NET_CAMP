2cecb9bf5e771b0aa393d57bcd1a1edbf11ebcaa
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-04-28 19:03:04 +0300

    6.2: add initial solution

---
+++ Home_task_6/Home_task_6/MultipleArraysProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_6
+{
+    internal class MultipleArraysProcessor
+    {
+        private int[][] _arrays;
+
+        public MultipleArraysProcessor(int[][] arrays)
+        {
+            int n = arrays.Length;
+            _arrays = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                int k = arrays[i].Length;
+                _arrays[i] = new int[k];
+                for (int j = 0; j < k; j++)
+                {
+                    _arrays[i][j] = arrays[i][j];
+                }
+            }
+        }
+
+        public IEnumerable<int> MergeAndSort()
+        {
+            List<int> merged = new List<int>();
+            for (int i = 0; i < _arrays.Length; i++)
+            {
+                merged.AddRange(_arrays[i]);
+            }
+            merged.Sort();
+
+            for (int i = 0; i < merged.Count; i++)
+            {
+                yield return merged[i];
+            }
+        }
+    }
+}
--- Home_task_6/Home_task_6/Program.cs
+++ Home_task_6/Home_task_6/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Task6_2();
+        }
+
+        static void Task6_2()
+        {
+            int[][] arrays1 = 
+            {
+                new int[] { 1,2,3 },
+                new int[] { 9,8,7 },
+                new int[] { -1,10,7 },
+            };
+            MultipleArraysProcessor arraysProc1 = new MultipleArraysProcessor(arrays1);
+            foreach (int item in arraysProc1.MergeAndSort())
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
