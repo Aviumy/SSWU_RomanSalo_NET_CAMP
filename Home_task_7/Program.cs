2febb7a9ab72f8a9aab5ad5e789754a63dd6eb4d
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-05-06 13:49:08 +0300

    Create project for home task 7

---
+++ Home_task_7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Home_task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
