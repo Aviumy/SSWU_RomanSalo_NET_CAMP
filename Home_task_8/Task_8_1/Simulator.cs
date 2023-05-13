450bb6e86ab164cdfc1ffb0539e8d3f6bad65247
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-05-13 14:22:27 +0300

    8.1: clone task 7 solution

---
+++ Home_task_8/Task_8_1/Simulator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_1
+{
+    internal class Simulator
+    {
+        private List<TrafficLight> _trafficLights;
+
+        public int SimulationTime { get; private set; }
+
+        public Simulator(List<TrafficLight> trafficLights)
+        {
+            _trafficLights = trafficLights.Select(x => x).ToList();
+            SimulationTime = 0;
+        }
+
+        public TrafficLight[] TrafficLights
+        {
+            get
+            {
+                return _trafficLights.Select(x => x).ToArray();
+            }
+        }
+
+        public void SimulateOneSecond()
+        {
+            SimulationTime++;
+            foreach (var trafficLight in _trafficLights)
+            {
+                trafficLight.DecrementStateTime();
+            }
+        }
+
+        public IEnumerable<uint> SimulateNSeconds(uint n)
+        {
+            for (uint i = 0; i < n; i++)
+            {
+                SimulateOneSecond();
+                yield return i + 1;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Час симуляції: {SimulationTime} с");
+            foreach (var trafficLight in _trafficLights)
+            {
+                result.Append(trafficLight + " | ");
+            }
+            result.AppendLine();
+            return result.ToString();
+        }
+    }
+}
---
+++ Home_task_8/Task_8_1/TrafficLight.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_1
+{
+    public enum State
+    {
+        Red,
+        RedAndYellow,
+        Green,
+        BlinkingGreen,
+        Yellow,
+    }
+
+    internal abstract class TrafficLight
+    {
+        protected ITrafficLightStrategy _strategy;
+        protected State[] _possibleStates;
+        protected Dictionary<State, uint> _stateTimes;
+
+        public event Action NewStateTimesIsNull;
+        public event Action NewStateTimesCountDoesntMatch;
+
+        protected TrafficLight(string name, State initialState, Dictionary<State, uint> stateSwitchTimes)
+        {
+            Name = name;
+            CurrState = initialState;
+
+            _stateTimes = new Dictionary<State, uint>();
+            foreach (var key in stateSwitchTimes.Keys)
+            {
+                _stateTimes.Add(key, stateSwitchTimes[key]);
+            }
+
+            StateTimeLeft = stateSwitchTimes[initialState];
+        }
+
+        public string Name { get; protected set; }
+        public State CurrState { get; protected set; }
+        public uint StateTimeLeft { get; protected set; }
+        public State[] PossibleStates
+        {
+            get
+            {
+                return _possibleStates.Select(x => x).ToArray();
+            }
+        }
+        public Dictionary<State, uint> StateTimes
+        {
+            get
+            {
+                var copy = new Dictionary<State, uint>();
+                foreach (var key in _stateTimes.Keys)
+                {
+                    copy.Add(key, _stateTimes[key]);
+                }
+                return copy;
+            }
+        }
+
+        public void SwitchState()
+        {
+            CurrState = _strategy.ChangeState(CurrState);
+            StateTimeLeft = _stateTimes[CurrState];
+        }
+
+        public void DecrementStateTime()
+        {
+            if (--StateTimeLeft == 0)
+            {
+                SwitchState();
+            }
+        }
+
+        public void ChangeStateTimes(uint[] newStateTimes)
+        {
+            if (newStateTimes == null)
+            {
+                NewStateTimesIsNull?.Invoke();
+            }
+            else
+            {
+                if (newStateTimes.Length != _possibleStates.Length)
+                {
+                    NewStateTimesCountDoesntMatch?.Invoke();
+                }
+                else
+                {
+                    for (int i = 0; i < newStateTimes.Length; i++)
+                    {
+                        _stateTimes[_possibleStates[i]] = newStateTimes[i];
+                        if (CurrState == _possibleStates[i])
+                        {
+                            StateTimeLeft = newStateTimes[i];
+                        }
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append($"{Name}: {CurrState} ({StateTimeLeft} с)");
+            return result.ToString();
+        }
+    }
+}
