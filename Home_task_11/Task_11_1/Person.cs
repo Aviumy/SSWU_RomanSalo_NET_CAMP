d0df4df273258499083368a381b35d1b1682fdd7
DESKTOP-4NIHT4U\Acer <saloroman23@gmail.com>
2023-06-12 22:51:55 +0300

    11.1: add tests with sorting an array of custom type, other minor changes to testing code

---
+++ Home_task_11/Task_11_1/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11_1
+{
+    internal class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public uint Age { get; set; }
+
+        public int CompareTo(Person? other)
+        {
+            if (this.Age < other.Age)
+            {
+                return -1;
+            }
+            else if (this.Age == other.Age)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
--- Home_task_11/Task_11_1/Program.cs
+++ Home_task_11/Task_11_1/Program.cs
@@ -12,7 +12,7 @@ namespace Task_11_1
         static void Task_11_1()
         {
             Random random = new Random();
-            int n = 200000;
+            int n = 100000;
 
             var intArrays = new List<int[]>() {
                 (new int[n]).Select(x => random.Next(-0, 11)).ToArray(),
@@ -27,9 +27,32 @@ namespace Task_11_1
                 stopwatch.Start();
                 Sorter<int>.QuickSort(intArrays[i], (Pivot)i);
                 stopwatch.Stop();
-                Console.WriteLine(stopwatch.ElapsedMilliseconds);
+                Console.WriteLine($"Pivot: {(Pivot)i}, elapsed time: {stopwatch.ElapsedMilliseconds} ms");
                 //PrintArray(intArrays[i]);
             }
+
+            Person[] people = new Person[]
+            {
+                new Person { Name = "Roman", Age = 21 },
+                new Person { Name = "Vitaliy", Age = 18 },
+                new Person { Name = "Alex", Age = 26 },
+                new Person { Name = "Oleh", Age = 15 },
+                new Person { Name = "Vasylyna", Age = 15 },
+                new Person { Name = "Olena", Age = 22 },
+            };
+
+            PrintArray(people);
+            Sorter<Person>.QuickSort(people, Pivot.MedianOf3);
+            PrintArray(people);
+        }
+
+        static void PrintArray<T>(T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
         }
     }
 }
