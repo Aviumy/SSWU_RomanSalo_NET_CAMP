b1206962a5820285d4eef55a91743c3eeac7d5bf
Roman Salo <saloroman23@gmail.com>
2023-07-10 02:37:35 +0300

    DB 2: add initial migration

---
+++ Home_task_DB_2/Migrations/202307092320042_Initial.cs
@@ -0,0 +1,68 @@
+﻿namespace Home_task_DB_2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Courseworks",
+                c => new
+                    {
+                        WorkId = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        WorkType = c.String(nullable: false),
+                        Subject = c.String(nullable: false),
+                        ApprovalDate = c.DateTime(nullable: false),
+                        PresentationDate = c.DateTime(nullable: false),
+                        Mark = c.Byte(nullable: false),
+                        StudentId = c.Int(),
+                        TeacherId = c.Int(),
+                    })
+                .PrimaryKey(t => t.WorkId)
+                .ForeignKey("dbo.Students", t => t.StudentId)
+                .ForeignKey("dbo.Teachers", t => t.TeacherId)
+                .Index(t => t.StudentId)
+                .Index(t => t.TeacherId);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentId = c.Int(nullable: false, identity: true),
+                        Firstname = c.String(nullable: false),
+                        Lastname = c.String(nullable: false),
+                        Middlename = c.String(nullable: false),
+                        Group = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.StudentId);
+            
+            CreateTable(
+                "dbo.Teachers",
+                c => new
+                    {
+                        TeacherId = c.Int(nullable: false, identity: true),
+                        Firstname = c.String(nullable: false),
+                        Lastname = c.String(nullable: false),
+                        Middlename = c.String(nullable: false),
+                        Position = c.String(nullable: false),
+                        Cathedra = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.TeacherId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courseworks", "TeacherId", "dbo.Teachers");
+            DropForeignKey("dbo.Courseworks", "StudentId", "dbo.Students");
+            DropIndex("dbo.Courseworks", new[] { "TeacherId" });
+            DropIndex("dbo.Courseworks", new[] { "StudentId" });
+            DropTable("dbo.Teachers");
+            DropTable("dbo.Students");
+            DropTable("dbo.Courseworks");
+        }
+    }
+}
---
+++ Home_task_DB_2/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Home_task_DB_2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Home_task_DB_2.Persistence.UniversityDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Home_task_DB_2.Persistence.UniversityDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
